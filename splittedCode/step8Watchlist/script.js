63890a8d72a832315d4d281c68abc0e00e15fcfd
octos-network <69785041+octos-network@users.noreply.github.com>
2024-04-10 15:32:12 +0200

    add step8

--- splittedCode/step8Watchlist/script.js
+++ splittedCode/step8Watchlist/script.js
@@ -3,33 +3,33 @@ let films = [];
 
 // function for adding a film
 function addFilm(event) {
-    
+
     // prevents the page from reloading after submitting the form
     event.preventDefault(); 
-    
+
     // read information from the text fields
     const titel = document.getElementById('titel').value;
     const genre = document.getElementById('genre').value;
     const mainActor = document.getElementById('mainActor').value;
     const length = parseInt(document.getElementById('length').value);
     const year = parseInt(document.getElementById('year').value);
-    const trailerLink = document.getElementById('trailerLink').value;
 
     // create film object and add to the "database" 
-    const film = { titel, genre, mainActor, length, year, trailerLink };
+    const film = { titel, genre, mainActor, length, year };
     films.push(film);
     showFilms();
+    watchlist.push(film);
+    showWatchlist();
 
-    // clears form
-    this.reset();
+     // clears form
+     this.reset();
 }
 
-// function for displaying the movies and event listeners for the Watchlist buttons
 function showFilms() {
 
     // fetches reference to the created div -> in HTML
     const filmList = document.getElementById('filmList');
-    
+ 
     // empties div
     filmList.innerHTML = '';
     
@@ -43,28 +43,16 @@ function showFilms() {
             <p>Main actor: ${film.mainActor}</p>
             <p>Length: ${film.length} minutes</p>
             <p>Publication year: ${film.year}</p>
-            <button onclick="watchFilm(event, ${index})">Watch Film</button>
-            <button onclick="filmToWatchlistEvent(event, ${index})">Add to Watchlist</button>
         `;
         filmList.appendChild(filmItem);
     });
-}
-
-// "database" for Watchlist
-let watchlist = [];
-
-// function for adding a film to the Watchlist
-function addFilmToWatchlist(index) {
-    const film = films[index];
-    watchlist.push(film);
-    showWatchlist();
 }
 
 // function for displaying the watchlist
 function showWatchlist() {
     const watchlist = document.getElementById('watchlist');
     watchlist.innerHTML = '';
-    watchlist.forEach((film, index) => {
+    watchlist.forEach((film) => {
         const watchlistItem = document.createElement('div');
         watchlistItem.classList.add('film');
         watchlistItem.innerHTML = `
@@ -73,42 +61,9 @@ function showWatchlist() {
             <p>Main actor: ${film.mainActor}</p>
             <p>Length: ${film.length} minutes</p>
             <p>Publication year: ${film.year}</p>
-            <button onclick="watchFilm(event, ${index})">Watch Film</button>
-            <button onclick="removeFilmFromWatchlist(${index})">Remove from Watchlist</button>
         `;
         watchlist.appendChild(watchlistItem);
     });
 }
 
-// event listener for adding a film to the watchlist
-function filmToWatchlistEvent(event, index) {
-    event.stopPropagation();
-    addFilmToWatchlist(index);
-}
-
-// Funktion zum Hinzufügen eines Films zur Watchlist
-function watchFilm(event, index) {
-    event.stopPropagation(); 
-    const film = films[index];
-    const trailerLink = film.trailerLink;
-    window.open(trailerLink, '_blank');
-}
-
-// function to remove a film from the Watchlist
-function removeFilmFromWatchlist(index) {
-    watchlist.splice(index, 1);
-    showWatchlist();
-}
-
-// va
-function validatePositiveInteger(event) {
-    const input = event.target.value;
-    if (!(/^\d+$/.test(input))) {
-        event.target.value = '';
-    }
-}
-
-// event listeners for the form
 document.getElementById('filmForm').addEventListener('submit', addFilm);
-document.getElementById('length').addEventListener('input', validatePositiveInteger);
-document.getElementById('year').addEventListener('input', validatePositiveInteger);
